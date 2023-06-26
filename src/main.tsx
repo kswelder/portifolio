72ecd2c53934fcd5da744fcfb06edab1a3335ad1
kswelder <welderhayne2000@gmail.com>
2023-06-25 22:46:07 -0300

    Adicionando react-router-dom e removendo arquivo .svg

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import App from './pages/App'
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' Component={App}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
