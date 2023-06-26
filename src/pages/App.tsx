dc06390967275ded7ac27ebf8940eeff0979e866
kswelder <welderhayne2000@gmail.com>
2023-06-25 22:47:49 -0300

    Adicionando header component e iniciando h1 com texto Home

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import HeaderNavComponent from '../components/HeaderNavComponent'
+import '../styles/App.css'
+
+
+function App() {
+  return (
+    <>
+      <HeaderNavComponent/>
+      <h1>Home</h1>
+    </>
+  )
+}
+
+export default App
