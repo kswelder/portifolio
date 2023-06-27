af3de9d7743c784b21d1b83bde00b4c82170f583
kswelder <welderhayne2000@gmail.com>
2023-06-27 05:13:58 -0300

    Pagina inicial de projetos em desenvolvimento

---
+++ src/pages/Projetos.tsx
@@ -0,0 +1,12 @@
+import HeaderNavComponent from "../components/HeaderNavComponent"
+
+function Projetos() {
+    return (
+        <>
+            <HeaderNavComponent/>
+            <h1>Projetos</h1>
+        </>
+    )
+}
+
+export default Projetos
