802408b5c7c14b59adcec109fbf86847e7dd9102
kswelder <welderhayne2000@gmail.com>
2023-06-27 05:10:24 -0300

    Adicionando nome de classe ao Link e uma div abaixo

--- src/components/HeaderNavComponent.tsx
+++ src/components/HeaderNavComponent.tsx
@@ -3,17 +3,24 @@ import "../styles/HeaderNavComponents.css"
 
 function HeaderNavComponent() {
     return (
-        <>
-            <header>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-        </>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/" className="link">Home</Link>
+                        <div></div>
+                    </li>
+                    <li>
+                        <Link to="/projetos" className="link">Projetos</Link>
+                        <div></div>
+                    </li>
+                    <li>
+                        <Link to="/sobre" className="link">Sobre</Link>
+                        <div></div>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
