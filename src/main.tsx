c4eaf5125da5099cb897763eca0d43e16b2b70f0
kswelder <welderhayne2000@gmail.com>
2023-06-27 05:13:28 -0300

    Adicionando pagina de projetos e sobre

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './pages/App'
+import Projetos from './pages/Projetos'
+import Sobre from './pages/Sobre'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
@@ -9,6 +11,8 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <BrowserRouter>
       <Routes>
         <Route path='/' Component={App}/>
+        <Route path='/projetos' Component={Projetos}/>
+        <Route path='/sobre' Component={Sobre}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
