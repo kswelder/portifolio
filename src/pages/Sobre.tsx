0aff26e09ab9b86c0d95e1e1a6fcd622da482c05
kswelder <welderhayne2000@gmail.com>
2023-06-27 05:14:15 -0300

    Pagina inicial sobre em desenvolvimento

---
+++ src/pages/Sobre.tsx
@@ -0,0 +1,13 @@
+import HeaderNavComponent from "../components/HeaderNavComponent"
+
+
+function Sobre() {
+    return (
+        <>
+            <HeaderNavComponent/>
+            <h1>Sobre</h1>
+        </>
+    )
+}
+
+export default Sobre
